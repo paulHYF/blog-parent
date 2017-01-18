478e401848a2f7e96ef7c50552e4590c5c6ab246
paulhyf <563025955@qq.com>
2017-01-18 16:10:59 +0800

    初始化框架

---
+++ src/main/java/com/paul/BlogParentApplication.java
@@ -0,0 +1,12 @@
+package com.paul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogParentApplication.class, args);
+	}
+}
